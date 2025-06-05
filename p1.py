f79874589c74db2c123d8e619081f1df66665547
Felipe González <gonzalezpobletefelipe@gmail.com>
2025-06-05 12:51:55 -0400

    modificacion de nombre

--- p1.py
+++ p1.py
@@ -6,41 +6,42 @@ from scipy.integrate import odeint
 omega = 314 # [rad/s]
 delta = 0.1 # [rad]
 
-def stator_inductance(theta):
+def L_11(theta):
     return 0.75 + 0.35 * np.cos(2*theta)
 
-def dot_stator_inductance(theta):
-    return -0.7*np.sin(2*theta)
+def dot_L_11(theta):
+    return -0.7*omega*np.sin(2*theta)
 
-def rotor_inductance(theta):
+def L_22(theta):
     return 0.5 + 0.2*np.cos(2*theta)
 
-def dot_rotor_inductance(theta):
-    return -0.4*np.sin(2*theta)
+def dot_L_22(theta):
+    return -0.4*omega*np.sin(2*theta)
 
-def mutual_inductance(theta):
+def L_12(theta):
     return 0.8*np.cos(theta)
 
-def dot_mutual_inductance(theta):
-    return -0.8*np.sin(theta)
+def dot_L_12(theta):
+    return -0.8*omega*np.sin(theta)
 
 def theta(t):
     return omega * t + delta
 
-def stator_current(t):
+def I_1(t):
     return np.sqrt(2)*np.sin(omega * t)
 
-def dot_stator_current(t):
+def dot_I_1(t):
     return omega*np.sqrt(2)*np.cos(omega*t)
 
-def dot_rotor_current(rotor_current,t):
-    a = -(stator_current(t)*dot_mutual_inductance(theta(t)))
-    b = -(dot_stator_current(t)*mutual_inductance(theta(t)))
-    c = -(rotor_current*dot_rotor_inductance(theta(t)))
-    return (a+b+c)/rotor_inductance(theta(t))
+def dot_rotor_current(I_2,t):
+    a = -(I_1(t)*dot_L_12(theta(t)))
+    b = -(dot_I_1(t)*L_12(theta(t)))
+    c = -(I_2*dot_L_22(theta(t)))
+    return (a+b+c)/L_22(theta(t))
 
 
-T = np.linspace(0,100*np.pi/omega,1000)
+T = np.linspace(0,6*np.pi/omega,1000)
 z = odeint(dot_rotor_current,0,T)
-plt.plot(T,z)
+plt.plot(T,z,label='Corriente en rotor')
+plt.xlabel('')
 plt.show()
--- p2.py
+++ p2.py
@@ -1,43 +1,38 @@
 import numpy as np
-from Calculator import N_steps, V_a
 import matplotlib.pyplot as plt
-R = np.linspace(0,2,N_steps)
+
 E_a = np.load('E_a.npy')
 I_a = np.load('I_a.npy')
 omega = np.load('omega.npy')
-
-# Aquí hay que graficar la corriente que circula por los motores
-# La velocidad del tren en régimen permanente v = (65 / (1000*2*pi/60))*omega
-# torque efectivo 12+2,2*omega
-# eficiencia del conjunto de motores 2*E_a/V_a
-# todo eso en funcion de R_s
-
+N_steps = len(E_a)
+V_a = 600 # [V]
+R = np.linspace(0,2,N_steps)
 eta = 2*E_a/V_a
 v = (65*60/(1000*2*np.pi))*omega
 tau = 12 + 2.2*omega
 
-plt.plot(R, I_a)
+plt.plot(R, I_a, color = 'red')
 plt.title('Corriente de armadura en funcion de resistencia variable')
 plt.xlabel(r'Resistencia [$\Omega$]')
 plt.ylabel('Corriente de armadura [A]')
 plt.grid(True)
 plt.show()
 
-plt.plot(R, tau)
+plt.plot(R, tau, color = 'green')
 plt.title('Torque efectivo en funcion de resistencia variable')
 plt.xlabel(r'Resistencia [$\Omega$]')
 plt.ylabel('Torque [Nm]')
 plt.grid(True)
 plt.show()
 
-plt.plot(R, eta)
+plt.plot(R, eta, color = 'blue')
 plt.title('Eficiencia de los motores en funcion de resistencia variable')
 plt.xlabel(r'Resistencia [$\Omega$]')
 plt.ylabel('Eficiencia')
 plt.grid(True)
 plt.show()
 
-plt.plot(R, v)
+plt.plot(R, v, color = 'orange')
 plt.title('Velocidad del tren en funcion de resistencia variable')
 plt.xlabel(r'Resistencia [$\Omega$]')
 plt.ylabel('Velocidad del tren [km/h]')
